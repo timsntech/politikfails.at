e127aced99662bd994bda955bbce79873ed0790c
dataluchs <tim_stehle@posteo.de>
2021-09-23 01:03:03 +0200

    add django-multiselectfield and adapt serializer

--- api/core/models.py
+++ api/core/models.py
@@ -1,15 +1,23 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 # Create your models here.
 
-class Snippet(models.Model):
-    CATEGORY_LEVELS = (
-        ('Korruption', 'Korruption'),
-        ('Verleumdung', 'Verleumdung'),
-        ('Betrug', 'Betrug'),
+CATEGORY_LEVELS = (
+        ('Bestechlichkeit', 'Bestechlichkeit'),
+        ('Antisemitismus', 'Antisemitismus'),
+        ('Amtsmissbrauch', 'Amtsmissbrauch'),
+        ('Falschaussage', 'Falschaussage'),
+        ('Gesetzwidrigkeit', 'Gesetzwidrigkeit'),
+        ('Verfassungswidrigkeit', 'Verfassungswidrigkeit'),
+        ('Unethisches Handeln', 'Unethisches Handeln'),
+        ('Unfähigkeit', 'Unfähigkeit'),
     )
+
+class Snippet(models.Model):
+
     name = models.CharField(max_length=120)
     description = models.CharField(max_length=1000)
-    category = models.CharField(choices=CATEGORY_LEVELS, max_length=20)
+    category = MultiSelectField(choices=CATEGORY_LEVELS, max_length=40)
     references = models.JSONField()
 
     def __str_(self):
--- api/core/serializers.py
+++ api/core/serializers.py
@@ -1,8 +1,10 @@
-from rest_framework import serializers
+from rest_framework import fields, serializers
 from .models import Snippet
+from .models import CATEGORY_LEVELS
 
 class SnippetSerializer(serializers.ModelSerializer):
-
+    category = fields.MultipleChoiceField(choices=CATEGORY_LEVELS)
+    
     class Meta:
         model = Snippet
         fields = ("id", "name", "description", "category", "references")
