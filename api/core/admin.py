067b39f2b1de15793ad83c9582155220bcfbcee3
dataluchs <tim_stehle@posteo.de>
2021-09-29 00:05:12 +0200

    django: add party and category urls and admin routes

--- api/core/admin.py
+++ api/core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Snippet 
+from .models import Snippet, Politician, Category, Party 
 # Register your models here.
 
 admin.site.register(Snippet)
+admin.site.register(Politician)
+admin.site.register(Category)
+admin.site.register(Party)
--- api/core/urls.py
+++ api/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SnippetViewSet, PolitikerViewSet
+from .views import SnippetViewSet, PoliticianViewSet, CategoryViewSet, PartyViewSet
 
 router = DefaultRouter()
 router.register(r'snippets', SnippetViewSet)
-router.register(r'politiker', PolitikerViewSet)
+router.register(r'politiker', PoliticianViewSet)
+router.register(r'partei', PartyViewSet)
+router.register(r'kategorie', CategoryViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
